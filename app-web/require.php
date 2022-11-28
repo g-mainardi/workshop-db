1323f1b83e387c2c28d11edce6a0df09febd660b
g-mainardi <gmainardi11@gmail.com>
2022-11-28 13:49:36 +0100

    Aggiunto array script in require

--- app-web/require.php
+++ app-web/require.php
@@ -5,6 +5,9 @@ session_start();
 require_once("database.php");
 $db = new DatabaseHelper("127.0.0.1", "root", "", "progetto_basi_di_dati", 3306);
 
-//Definisco costanti
+// Definisco costanti
 define("UPLOAD_DIR", "./upload/");
+
+// Creo array per gli scripts
+$SetParameters["scripts"] = array ();
 ?>
