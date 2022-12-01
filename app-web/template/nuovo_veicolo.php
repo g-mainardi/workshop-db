de7cf0f1916959482b2ca8e11e20255104b8382f
elisa318 <56109343+elisa318@users.noreply.github.com>
2022-12-01 09:23:21 +0100

    Update nuovo_veicolo.php

--- app-web/template/nuovo_veicolo.php
+++ app-web/template/nuovo_veicolo.php
@@ -1,46 +1,59 @@
 <section>
-	<h2>Registra un veicolo</h2>
+	<h2>Registra un cliente</h2>
 	<form action="#" method="POST">
 		<ul>
 			<li>
-				<label for="veicoloCasaProd"> Casa di Produzione: </label>
-				<input type="text" name="veicoloCasaProd" placeholder="Casa di Produzione" maxlength="16" required>
+				<label for="clienteCF"> Codice fiscale: </label>
+				<input type="text" name="clienteCF" placeholder="Codice fiscale" maxlength="16" required>
 			</li>
 			<li>				
-				<label for="veicoloModello"> Modello: </label>
-				<input type="text" name="veicoloModello" placeholder="Modello" maxlength="20" required>
+				<label for="clienteNome"> Nome: </label>
+				<input type="text" name="clienteNome" placeholder="Nome" maxlength="20" required>
+			</li>
+			<li>
+				<label for="clienteCognome"> Cognome: </label>
+				<input type="text" name="clienteCognome" placeholder="Cognome" maxlength="20" required>
 			</li>
 			<li>				
-				<label for="veicoloAnnoProd"> Anno di Produzione: </label>
-				<input type="number" min="1960" max="2022" name="veicoloAnnoProd" placeholder="Anno" required>
+				<label for="clienteDataNascita"> Data di nascita: </label>
+				<input type="date" name="clienteDataNascita" required>
 			</li>
 			<li>				
-				<label for="veicoloCilindrata"> Cilindrata: </label>
-				<input type="number" maxlength="10" name="veicoloCilindrata" placeholder="Cilindrata" required>
+				<label for="clienteTelefono"> Telefono: </label>
+				<input type="tel" pattern="[0-9]{10}" maxlength="10" name="clienteTelefono" placeholder="Telefono" required>
+			</li>
+			<li>				
+				<label for="clienteMail"> Email(opzionale): </label>
+				<input type="email" name="clienteMail" placeholder="Email" maxlength="80">
 			</li>
 		</ul>		
-		<input type="submit" name="submit"  value="Inserisci veicolo">
+		<input type="submit" name="inserisciCliente"  value="Inserisci cliente">
 	</form>
 
 </section>
 
 <section class="tabelle">
-	<h2>Veicoli registrati</h2>
+	<h2>Clienti registrati</h2>
+	<p>È possibile modificare Telefono e Mail di un cliente</p>
 	<table>
 		<thead>
 			<tr>
-				<th>CASA PRODUZIONE</th><th>MODELLO</th><th>ANNO DI PRODUZIONE</th><th>CILINDRATA(CM3)</th><th>PROPRIETARIO</th>
+				<th>CF</th><th>NOME</th><th>COGNOME</th><th>DATA DI NASCITA</th><th>TELEFONO</th><th>MAIL</th><th>AGGIORNA</th>
 			</tr>
 		</thead>
 		<tbody>
-			<?php foreach($SetParameters["veicoli"] as $veicolo) :?>
+			<?php foreach($SetParameters["clienti"] as $cliente) :?>
+			<form action="#" method="POST">
 				<tr>
-					<td><?php echo $veicolo["casaProd"]; ?></td>
-					<td><?php echo $veicolo["modello"]; ?></td>
-					<td><?php echo $veicolo["anno_prod"]; ?></td>
-					<td><?php echo $veicolo["cilindrata"]; ?></td>
-					<td><?php echo $veicolo["proprietario"]?></td>
+					<td><input type="hidden" name="clienteCF" value=<?php echo $cliente["codice_fiscale"]; ?>> <?php echo $cliente["codice_fiscale"]?></td>
+					<td><input type="hidden" name="clienteNome" value=<?php echo $cliente["nome"]; ?>> <?php echo $cliente["nome"]?></td>
+					<td><input type="hidden" name="clienteCognome" value=<?php echo $cliente["cognome"]; ?>> <?php echo $cliente["cognome"]?></td>
+					<td><input type="hidden" name="clienteDataNascita" value=<?php echo $cliente["data_nascita"]; ?>> <?php echo $cliente["data_nascita"]?></td>
+					<td><input type="tel" pattern="[0-9]{10}" maxlength="10" name="clienteTelefono" placeholder="Inserisci Telefono" value=<?php echo $cliente["telefono"]?> required></td>
+					<td><input type="email" maxlength="80" name="clienteMail" placeholder="Inserisci email" value=<?php echo $cliente["email"]?>></td>
+					<td><input disabled type="submit" name="aggiornaTelefono" value="Aggiorna Telefono"> <input disabled type="submit" name="aggiornaMail" value="Aggiorna Mail"><input disabled type="submit" name="aggiornaTutto" value="Aggiorna Tutto"> </td>
 				</tr>
+			</form>
 			<?php endforeach; ?>
 		</tbody>
 	</table>
