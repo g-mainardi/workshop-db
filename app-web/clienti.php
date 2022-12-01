69818b06f1efc5f0c8bc081025017f942249ecb3
elisa318 <56109343+elisa318@users.noreply.github.com>
2022-12-01 09:25:57 +0100

    Update clienti.php

--- app-web/clienti.php
+++ app-web/clienti.php
@@ -7,84 +7,62 @@
 	array_push($SetParameters["scripts"], "./js/script.js");
 
 	// Leggo clienti dal database
-	$SetParameters["clienti"] = [array("CF" => "RSSMRA65P21R889T", "nome" => "mario",
-									"cognome" => "rossi",	"telefono" => "3428990332", 
-									"data_nascita" => "1965-10-21", "mail" => ""),
-									array("CF" => "RSSMRA65P21R889T", "nome" => "mario",
-									"cognome" => "rossi",	"telefono" => "3428990332", 
-									"data_nascita" => "1965-10-21", "mail" => "rossi.mario@gmail.com"),
-									array("CF" => "RSSMRA65P21R889T", "nome" => "mario",
-									"cognome" => "rossi",	"telefono" => "3428990332", 
-									"data_nascita" => "1965-10-21", "mail" => "rossi.mario@gmail.com"),
-									array("CF" => "RSSMRA65P21R889T", "nome" => "mario",
-									"cognome" => "rossi",	"telefono" => "3428990332", 
-									"data_nascita" => "1965-10-21", "mail" => "rossi.mario@gmail.com"),
-									array("CF" => "RSSMRA65P21R889T", "nome" => "mario",
-									"cognome" => "rossi",	"telefono" => "3428990332", 
-									"data_nascita" => "1965-10-21", "mail" => "rossi.mario@gmail.com")];  // $db->getClienti()
+	$SetParameters["clienti"] = $db->getClients();
 	
 	if(isset($_POST["aggiornaTelefono"])){
-		/*
-		$db->updateClientTelephone($_POST["clienteCF"], $_POST["clienteTelefono"]);
-		*/
-		echo "aggiornato: ".$_POST["clienteCF"]." numero: ".$_POST["clienteTelefono"];
+		$type = $db->getDependentType($_POST["clienteCF"]);
+		$db->updateTelephone( $_POST["clienteTelefono"], $_POST["clienteCF"], $type);
+		#echo "aggiornato: ".$_POST["clienteCF"]." numero: ".$_POST["clienteTelefono"];
 	}
 	if(isset($_POST["aggiornaMail"])){
-		/*
-		$db->updateClientMail($_POST["clienteCF"], $_POST["clienteMail"]);
-		*/
-		echo "aggiornato: ".$_POST["clienteCF"]." Mail: ".$_POST["clienteMail"];
+		$type = $db->getDependentType($_POST["clienteCF"]);
+		$db->updateEmail($_POST["clienteMail"], $_POST["clienteCF"], $type);
+		echo "<meta http-equiv='refresh' content='0'>";
+		
+		#echo "aggiornato: ".$_POST["clienteCF"]." Mail: ".$_POST["clienteMail"];
 	}
 	if(isset($_POST["aggiornaTutto"])){
-		/*
-		$db->updateClient($_POST["clienteCF"], $_POST["clienteTelefono"], $_POST["clienteMail"]);
-		*/
-		echo "aggiornato: ".$_POST["clienteCF"]." numero: ".$_POST["clienteTelefono"]." Mail: ".$_POST["clienteMail"];
+		$db->updateEverythingClient($_POST["clienteMail"], $_POST["clienteTelefono"], $_POST["clienteCF"]);
+		#echo "aggiornato: ".$_POST["clienteCF"]." numero: ".$_POST["clienteTelefono"]." Mail: ".$_POST["clienteMail"];
 	}
-
-    if(isset($_POST["clienteCF"]) && isset($_POST["clienteNome"]) && isset($_POST["clienteCognome"]) 
+	
+	if(isset($_POST["inserisciCliente"])){
+		if(isset($_POST["clienteCF"]) && isset($_POST["clienteNome"]) && isset($_POST["clienteCognome"]) 
 		&& isset($_POST["clienteTelefono"]) && isset($_POST["clienteDataNascita"]))
-	{
-		$error = false;
-		// Controllo se sono inseriti nome e cognome
-		if(strlen($_POST['clienteNome']) <= 0 || strlen($_POST['clienteCognome']) <= 0){
-			echo "Nome e cognome non possono essere vuoti<br>";
-			$error = true;
-		}
-		// Controllo se mail inserita correttamente
-		if($_POST["clienteMail"] != "" && !filter_var($_POST['clienteMail'], FILTER_VALIDATE_EMAIL)){
-			echo "L'email non ha un giusto formato<br>" . $_POST["clienteMail"];
-			$error = true;
-		}
-		// Controllo se CF inserito correttamente
-		if(strlen($_POST['clienteCF']) != 16){
-			echo "Il codice fiscale è composto da esattamente 16 caratteri<br>";
-			$error = true;
-		}
-		// Controllo se telefono inserito correttamente
-		if(!is_numeric($_POST['clienteTelefono'])){
-			echo "Numero di telefono non valido<br>";
-			$error = true;
-		}
-		/*
-		// Controllo se CF già presente
-		if($db->checkAll(($_POST['clienteCF']))!=0)
 		{
-			echo "CF gia presente nel database<br>";
-			$error = true;
-		}
-		*/
-		if(!$error){
-			// Inserisco cliente nel database
-			/*
-			$db->insertCliente($_POST["clienteCF"], $_POST["clienteNome"], $_POST["clienteCognome"],
-					$_POST["clienteTelefono"], $_POST["clienteDataNascita"], $_POST["clienteMail"]);
-			*/
-			$SetParameters["clienti"][] = array("CF" => $_POST["clienteCF"], "nome" => $_POST["clienteNome"],
-										"cognome" => $_POST["clienteCognome"],	"telefono" => $_POST["clienteTelefono"], 
-										"data_nascita" => $_POST["clienteDataNascita"], "mail" => $_POST["clienteMail"]);
-		}
-    }
+			$error = false;
+			// Controllo se sono inseriti nome e cognome
+			if(strlen($_POST['clienteNome']) <= 0 || strlen($_POST['clienteCognome']) <= 0){
+				echo "Nome e cognome non possono essere vuoti<br>";
+				$error = true;
+			}
+			// Controllo se mail inserita correttamente
+			if($_POST["clienteMail"] != "" && !filter_var($_POST['clienteMail'], FILTER_VALIDATE_EMAIL)){
+				echo "L'email non ha un giusto formato<br>" . $_POST["clienteMail"];
+				$error = true;
+			}
+			// Controllo se CF inserito correttamente
+			if(strlen($_POST['clienteCF']) != 16){
+				echo "Il codice fiscale è composto da esattamente 16 caratteri<br>";
+				$error = true;
+			}
+			// Controllo se telefono inserito correttamente
+			if(!is_numeric($_POST['clienteTelefono'])){
+				echo "Numero di telefono non valido<br>";
+				$error = true;
+			}
+			if(!$error){
+				// Inserisco cliente nel database
+				try{
+					$db->insertCliente($_POST["clienteCF"], $_POST["clienteNome"], $_POST["clienteCognome"],
+					$_POST["clienteDataNascita"], $_POST["clienteTelefono"], $_POST["clienteMail"]);
+				}catch (Exception $e) {
+					echo 'Errore: è stato inserito un utente già presente. ';
+				}
+				
+			}
+    	}
+	}
 
     require("template/base.php");
 
