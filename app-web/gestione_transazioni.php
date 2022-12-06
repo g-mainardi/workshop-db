4192f8bff70fcb47bef09b785eeb8ec635759604
elisa318 <56109343+elisa318@users.noreply.github.com>
2022-12-06 11:37:53 +0100

    Update gestione_transazioni

--- app-web/gestione_transazioni.php
+++ app-web/gestione_transazioni.php
@@ -4,44 +4,46 @@
 
     $SetParameters["titolo"] = "Gestione transazioni";
     $SetParameters["file"] = "nuova_transazione.php";
-    $SetParameters["agenti"] = $db->getAgents();
-	$SetParameters["clienti"] = $db->getClients();
-	
+    
 	$SetParameters["selezionati"] = false;
 
 	// Fase controllo primi 3 input: Agente, Cliente e Tipo di transazione
 	if(isset($_POST["avanti"]) && isset($_POST["selezioneAgente"]) && isset($_POST["selezioneCliente"]) && isset($_POST["selezioneTipo"])){
 		$SetParameters["selezionati"] = true;
-		$SetParameters["agente"] = ["codice_fiscale" => $_POST["selezioneAgente"], "nome" => "mario", "cognome" => "rossi"];   // getUser($_POST["selezioneAgente"]) # ottengo agente da cf
-		$SetParameters["cliente"] = ["codice_fiscale" => $_POST["selezioneCliente"], "nome" => "mario", "cognome" => "rossi"]; // getUser($_POST["selezioneCliente"]) # ottengo cliente da cf
+		$_SESSION["selezionati"] = true;
+		$SetParameters["agente"] = $db->checkAgent($_POST["selezioneAgente"]);
+		$_SESSION["agente"] = $SetParameters["agente"][0];
+		$SetParameters["cliente"] = $db->checkClient($_POST["selezioneCliente"]); 
+		$_SESSION["cliente"] = $SetParameters["cliente"][0];
 		$SetParameters["tipo"] = $_POST["selezioneTipo"];
-		if($SetParameters["tipo"] == "acquisto"){
-			# questo è un array fittizio per provare senza funzione db
-			$SetParameters["veicoli"] = [
-				array("casaProd" => "Fiat", "modello" => "Stilo"),
-				array("casaProd" => "Audi", "modello" => "A4"),
-				array("casaProd" => "Volkswagen", "modello" => "Golf")
-			]; 
-			//$dbh->getClientVehicles($SetParameters["cliente"]); # i veicoli del cliente
+		$_SESSION["tipo"] = $SetParameters["tipo"];
+		if($_POST["selezioneTipo"] == "acquisto"){
+			$scaduto = 0;
+			$SetParameters["veicoli"] = $db->getClientValidCar($_POST["selezioneCliente"], $scaduto);
 		} else{
-			# questo è un array fittizio per provare senza funzione db
-			$SetParameters["veicoli"] = [
-			array("casaProd" => "Citroen", "modello" => "C3"),
-			array("casaProd" => "Toyota", "modello" => "Yaris")
-			]; 
-			//$dbh->getWorkshopVehicles(); # i veicoli dell'officina
+			$scaduto = 1;
+			$SetParameters["veicoli"] = $db->getGarageCar($scaduto); # i veicoli dell'officina
 		}
 	}
-
-	if($SetParameters["selezionati"] && isset($_POST["inserisciTransazione"]) && isset($_POST["selezioneVeicolo"]) && isset($_POST["selezionePrezzo"])){
-		# effettuo il cambio di proprietà (scade la vecchia e ne creo una nuova)
-		//$db->changeCarOwner($_POST["selezioneVeicolo"], $_POST["selezioneCliente"]);	
-
-		# creo la transazione
-		//$db->insertTransaction($_POST["selezioneAgente"],$_POST["selezioneCliente"],$_POST["selezioneVeicolo"]), -data_attuale-, $_POST["selezioneTipo"],$_POST["selezionePrezzo"]);
+	else if($_SESSION["selezionati"] && isset($_POST["inserisciTransazione"]) && isset($_POST["selezioneVeicolo"]) && isset($_POST["selezionePrezzo"])){
+		$_SESSION["selezionati"] = false;
+		$data_produzione = date('Y-m-d');
+		if($_SESSION["tipo"]  == "acquisto"){
+			$scaduto=1;
+			$db->updateCertificate($scaduto, $_POST["selezioneVeicolo"], $_SESSION["cliente"]["codice_fiscale"]);
+			#problema non mi refresha la pagina
+		}else{
+			$scaduto = 0;
+			# effettuo il cambio di proprietà, comprando dall'officin vuol dire che l'attestato è già cambiato quindi (scade la vecchia e ne creo una nuova)
+			$db->insertOwnershipCertificate($_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"], $scaduto, $data_produzione);
+		}
+		$db->insertTransaction($_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"], $_SESSION["agente"]["codice_fiscale"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"]);
 	}
-
-	$SetParameters["transazioni"] = array(); // da implementare $db->getAllTransactions();
+	else{
+		$SetParameters["agenti"] = $db->getAllAgents();
+		$SetParameters["clienti"] = $db->getAllClient();
+	}
+	$SetParameters["transazioni"] = $db->getAllTransactions(); // da implementare $db->getAllTransactions();
     require("template/base.php");
 
 ?>
