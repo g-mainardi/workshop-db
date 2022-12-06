8a6c2703ea8c8b98c163be8fa4cfdd98bcf58e9b
elisa318 <56109343+elisa318@users.noreply.github.com>
2022-12-06 11:39:44 +0100

    Update nuovo_meccanico.php

--- app-web/template/nuovo_meccanico.php
+++ app-web/template/nuovo_meccanico.php
@@ -49,13 +49,13 @@
 			<?php foreach($SetParameters["meccanici"] as $meccanico) :?>
 			<form action="#" method="POST">
 				<tr>
-					<td><input type="hidden" name="meccanicoCF" value=<?php echo $meccanico["CF"]; ?>> <?php echo $meccanico["CF"]?></td>
+					<td><input type="hidden" name="meccanicoCF" value=<?php echo $meccanico["codice_fiscale"]; ?>> <?php echo $meccanico["codice_fiscale"]?></td>
 					<td><input type="hidden" name="meccanicoNome" value=<?php echo $meccanico["nome"]; ?>> <?php echo $meccanico["nome"]?></td>
 					<td><input type="hidden" name="meccanicoCognome" value=<?php echo $meccanico["cognome"]; ?>> <?php echo $meccanico["cognome"]?></td>
 					<td><input type="hidden" name="meccanicoDataNascita" value=<?php echo $meccanico["data_nascita"]; ?>> <?php echo $meccanico["data_nascita"]?></td>
 					<td><input type="tel" pattern="[0-9]{10}" maxlength="10" name="meccanicoTelefono" placeholder="Inserisci Telefono" value=<?php echo $meccanico["telefono"]?> required></td>
-					<td><input type="email" maxlength="80" name="meccanicoMail" placeholder="Inserisci email" value=<?php echo $meccanico["mail"]?>></td>
-					<td><input type="number" min="1" name="meccanicoPaga" placeholder="Inserisci paga oraria" value=<?php echo $meccanico["paga"]?> required></td>
+					<td><input type="email" maxlength="80" name="meccanicoMail" placeholder="Inserisci email" value=<?php echo $meccanico["email"]?>></td>
+					<td><input type="number" min="1" name="meccanicoPaga" placeholder="Inserisci paga oraria" value=<?php echo $meccanico["paga_oraria"]?> required></td>
 					<td> <input disabled type="submit" name="aggiornaTelefono" value="Aggiorna Telefono"> <input disabled type="submit" name="aggiornaMail" value="Aggiorna Mail"> <input disabled type="submit" name="aggiornaPaga" value="Aggiorna Paga"> <input disabled type="submit" name="aggiornaTutto" value="Aggiorna Tutto"> </td>
 				</tr>
 			</form>
