dff4440c36c5e94a15061a61fb02219cfc9a3d20
g-mainardi <gmainardi11@gmail.com>
2022-12-09 00:24:59 +0100

    Forgotten file for previous commit

--- app-web/js/script.js
+++ app-web/js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(() => {
     $("input[type='tel']").change(function(e){
         e.preventDefault();
-        $("input[name='aggiornaTelefono']").removeAttr("disabled");
-        $("input[name='aggiornaTutto']").removeAttr("disabled");
+        const id = $(this).attr('id'); 
+        $("input[name='aggiornaTelefono'][id='"+ id +"']").removeAttr("disabled");
+        $("input[name='aggiornaTutto'][id='"+ id +"']").removeAttr("disabled");
     })
 
     $("input[type='email']").change(function(e){
         e.preventDefault();
-        $("input[name='aggiornaMail']").removeAttr("disabled");
-        $("input[name='aggiornaTutto']").removeAttr("disabled");
+        const id = $(this).attr('id'); 
+        $("input[name='aggiornaMail'][id='"+ id +"']").removeAttr("disabled");
+        $("input[name='aggiornaTutto'][id='"+ id +"']").removeAttr("disabled");
     })
 
     $("input[type='number']").change(function(e){
         e.preventDefault();
-        $("input[name='aggiornaPaga']").removeAttr("disabled");
-        $("input[name='aggiornaTutto']").removeAttr("disabled");
+        const id = $(this).attr('id'); 
+        $("input[name='aggiornaPaga'][id='"+ id +"']").removeAttr("disabled");
+        $("input[name='aggiornaTutto'][id='"+ id +"']").removeAttr("disabled");
     })
 });
