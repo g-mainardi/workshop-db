60734b82e8966ff4da012d28d2c2df1714a93a0f
elisa318 <56109343+elisa318@users.noreply.github.com>
2023-01-04 10:26:42 +0100

    Update gestione_transazioni.php

--- app-web/gestione_transazioni.php
+++ app-web/gestione_transazioni.php
@@ -21,25 +21,10 @@
 		$SetParameters["tipo"] = $_POST["selezioneTipo"];
 		$_SESSION["tipo"] = $SetParameters["tipo"];
 		if($_POST["selezioneTipo"] == "acquisto"){
-			$scaduto = 0;
-			$SetParameters["veicoli"] = $db->getClientValidCar($_POST["selezioneCliente"], $scaduto);
+			$SetParameters["veicoli"] = $db->getClientValidCar(0, $_POST["selezioneCliente"]);
 		} else{
 			$scaduto = 1;
-			$arrayProva =  $db->getGarageCar(1);
-			$SetParameters["veicoli"] = array();
-			$arrayname = array();
-			for ($i = 0; $i < count($arrayProva); ++$i) {
-				if (!in_array($arrayProva[$i]["cod_veicolo"], $arrayname)){
-					array_push($arrayname, $arrayProva[$i]["cod_veicolo"]);
-					$result = $db->checkActiveCertificate($arrayProva[$i]["cod_veicolo"], 0);
-					if (!empty($result)){
-						array_splice($arrayProva, $i, 1);
-						#unset($arrayProva[$i]);
-					}else{
-						array_push($SetParameters["veicoli"], $arrayProva[$i]);
-					}
-				}
-			}
+			$SetParameters["veicoli"] =  $db->getGarageCar(1);
 		}
 	}
 	else if($_SESSION["selezionati"] && isset($_POST["inserisciTransazione"]) && isset($_POST["selezioneVeicolo"]) && isset($_POST["selezionePrezzo"])){
@@ -47,12 +32,10 @@
 		$data_produzione = date('Y-m-d');
 		if($_SESSION["tipo"]  == "acquisto"){
 			$scaduto=1;
-			$db->updateCertificate($scaduto, $_POST["selezioneVeicolo"], $_SESSION["cliente"]["codice_fiscale"]);
-			#problema non mi refresha la pagina
+			$db->updateCertificate($scaduto, $_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"]);
 		}else{
-			$scaduto = 0;
-			# effettuo il cambio di proprietà, comprando dall'officin vuol dire che l'attestato è già cambiato quindi (scade la vecchia e ne creo una nuova)
-			$db->insertOwnershipCertificate($_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"], $scaduto, $data_produzione);
+			$scaduto=0;
+			$db->updateCertificate($scaduto, $_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"]);
 		}
 		$db->insertTransaction($_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"], $_SESSION["agente"]["codice_fiscale"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"]);
 	}
