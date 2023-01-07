85858f1725ac7f75551ec60af80550892f38153d
elisa318 <56109343+elisa318@users.noreply.github.com>
2023-01-07 12:38:01 +0100

    Create sezioni_veicoli.php

---
+++ app-web/template/sezioni_veicoli.php
@@ -0,0 +1,12 @@
+<section class="prova">
+	<ul>
+        <li>
+			<a href="gestione_veicolo_nuovo.php">VEICOLI NUOVI</a>
+			<p>Qui puoi visualizzare la lista dei veicoli nuovi presenti, e registrarne uno nuovo </p>
+		</li>
+		<li>
+			<a href="gestione_veicolo_usato.php">VEICOLI USATI</a>
+			<p>Qui puoi visualizzare la lista dei veicoli usati presenti, e registrarne uno nuovo </p>
+		</li>
+	</ul>
+</section>
