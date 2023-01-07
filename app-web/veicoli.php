f0a03d9b6cec3af4f888f545995917f530a48c04
elisa318 <56109343+elisa318@users.noreply.github.com>
2023-01-07 17:33:22 +0100

    Create veicoli.php

---
+++ app-web/veicoli.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once 'require.php';
+
+    $SetParameters["titolo"] = "Veicoli nuovi ed usati";
+    $SetParameters["file"] = "sezioni_veicoli.php";
+    require("template/base.php");
+
+?>
