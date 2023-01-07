d6c416b5029ba82052b0e85b86b3d7649244e903
elisa318 <56109343+elisa318@users.noreply.github.com>
2023-01-07 13:40:50 +0100

    Update gestione_transazioni.php

--- app-web/gestione_transazioni.php
+++ app-web/gestione_transazioni.php
@@ -5,7 +5,7 @@
     $SetParameters["titolo"] = "Gestione transazioni";
     $SetParameters["file"] = "nuova_transazione.php";
     
-	$SetParameters["selezionati"] = false;
+	$SetParameters["selezionati_specifiche"] = false;
 	if (!isset($_SESSION["selezionati"])) {
 		$_SESSION["selezionati"] = false;	
 	}
@@ -20,30 +20,67 @@
 		$_SESSION["cliente"] = $SetParameters["cliente"][0];
 		$SetParameters["tipo"] = $_POST["selezioneTipo"];
 		$_SESSION["tipo"] = $SetParameters["tipo"];
+		$SetParameters["tipoVeicolo"] = $_POST["selezioneTipoVeicolo"];
+		$_SESSION["tipoVeicolo"] = $SetParameters["tipoVeicolo"];
 		if($_POST["selezioneTipo"] == "acquisto"){
-			$SetParameters["veicoli"] = $db->getClientValidCar(0, $_POST["selezioneCliente"]);
+			if($_SESSION["tipoVeicolo"] == "nuovo"){
+				$SetParameters["veicoli"] = $db->getAllVeicoliNuovi();
+			}else{
+				$SetParameters["veicoli"] = $db->getClientValidCar(0, $_POST["selezioneCliente"]);
+			}
 		} else{
-			$scaduto = 1;
-			$SetParameters["veicoli"] =  $db->getGarageCar(1);
+			if($_SESSION["tipoVeicolo"] == "nuovo"){
+				$SetParameters["veicoli"] = $db->getAllVeicoliNuovi();
+			}else{
+				#TUTTI I VEICOLI USATI CHE SONO DI PROPRIETà DELL'OFFICINA
+				$SetParameters["veicoli"] = $db->getGarageCar(1);
+			}
 		}
 	}
 	else if($_SESSION["selezionati"] && isset($_POST["inserisciTransazione"]) && isset($_POST["selezioneVeicolo"]) && isset($_POST["selezionePrezzo"])){
 		$_SESSION["selezionati"] = false;
 		$data_produzione = date('Y-m-d');
+		$ora_transazione = date("h:i:sa");
 		if($_SESSION["tipo"]  == "acquisto"){
-			$scaduto=1;
-			$db->updateCertificate($scaduto, $_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"]);
+			if($_SESSION["tipoVeicolo"]=="usato"){
+				print_r("sono dentro");
+				$scaduto=1;
+				$db->updateCertificate($scaduto, $_SESSION["cliente"]["CF_cliente"], $_POST["selezioneVeicolo"]);
+				$db->insertTransaction($_SESSION["cliente"]["CF_cliente"], $_POST["selezioneVeicolo"], $_SESSION["agente"]["CF_agente"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"], $ora_transazione);
+			}
 		}else{
-			$scaduto=0;
-			$db->updateCertificate($scaduto, $_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"]);
+			if($_SESSION["tipoVeicolo"]=="nuovo"){
+				#elimino il veicolo da i veicoli nuovi
+				$informazioni_veicolo = $db->getVeicoloNuovo($_POST["selezioneVeicolo"]);
+				$modello =$informazioni_veicolo[0]["modello"];
+				$casa_produttrice = $informazioni_veicolo[0]["casa_produttrice"];
+				$anno_produzione =$informazioni_veicolo[0]["anno_produzione"];
+				$cilindrata = $informazioni_veicolo[0]["cilindrata"];
+				$km_percorsi = 0;
+				$scaduto = 0;
+				#in tutte le transaioni con tipo veicolo = nuovo e cod_veiolo = $_POST["selezioneVeicolo"]
+				#devo modificare il nuo
+				$db->deleteNuovoVeicolo($_POST["selezioneVeicolo"]);
+				print_r("eliminazione");
+				#inserisco il veicolo in quelli usati con km ercodi = 0 
+				$db->insertVeicoloUsato($casa_produttrice, $modello, $anno_produzione, $cilindrata, $km_percorsi);
+				print_r("veicolo nuovo");
+				$cod_veicolo = $db->getCarCod( $modello, $casa_produttrice,$anno_produzione, $cilindrata,$km_percorsi);
+				$db->insertAttestatoProprieta($_SESSION["cliente"]["CF_cliente"], $cod_veicolo, $scaduto, $data_produzione);
+				print_r("inserito attestato");
+				$db->insertTransaction($_SESSION["cliente"]["CF_cliente"], $cod_veicolo, $_SESSION["agente"]["CF_agente"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"], $ora_transazione);
+			}else{
+				$scaduto=0;
+				$db->updateCertificate($scaduto, $_SESSION["cliente"]["CF_cliente"], $_POST["selezioneVeicolo"]);
+				$db->insertTransaction($_SESSION["cliente"]["CF_cliente"], $_POST["selezioneVeicolo"], $_SESSION["agente"]["CF_agente"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"], $ora_transazione);
+			}
 		}
-		$db->insertTransaction($_SESSION["cliente"]["codice_fiscale"], $_POST["selezioneVeicolo"], $_SESSION["agente"]["codice_fiscale"],  $_POST["selezionePrezzo"], $data_produzione , $_SESSION["tipo"]);
 	}
 	else{
 		$SetParameters["agenti"] = $db->getAllAgents();
 		$SetParameters["clienti"] = $db->getAllClient();
 	}
-	$SetParameters["transazioni"] = $db->getAllTransactions(); // da implementare $db->getAllTransactions();
+	$SetParameters["transazioni"] = $db->getAllTransactions(); 
     require("template/base.php");
 
 ?>
