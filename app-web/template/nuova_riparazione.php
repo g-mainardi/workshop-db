a33891924d8a15725722d2a8c0a652c2bd8e9168
elisa318 <56109343+elisa318@users.noreply.github.com>
2023-01-08 22:05:05 +0100

    Update nuova_riparazione.php

--- app-web/template/nuova_riparazione.php
+++ app-web/template/nuova_riparazione.php
@@ -15,17 +15,17 @@
 				<?php endif;?>
 				</select>
 			</li>
-			
+		
 			<?php if($_SESSION["selezionatoCliente"]): ?>
 			<li>				
 				<label for="selezioneVeicolo"> Seleziona Veicolo: </label>
 				<?php if($_SESSION["selezionatoVeicolo"]):?>
 					<select name="selezioneVeicolo" required disabled>
-						<option value=<?php echo $_SESSION["veicoloCod"]?>><?php echo $_SESSION["veicoloSpecifiche"]?></option>
+						<option value=<?php echo $_SESSION["veicoloTarga"]?>><?php echo $_SESSION["veicoloSpecifiche"]?></option>
 				<?php else:?>
 					<select name="selezioneVeicolo" required>
 					<?php foreach($_SESSION["veicoli"] as $veicolo) :?>
-						<option value=<?php echo $veicolo["cod_veicolo_usato"]?>><?php echo $veicolo["casa_produttrice"]." - ".$veicolo["modello"]." (".$veicolo["anno_produzione"].")"?></option>
+						<option value=<?php echo $veicolo["targa"]?>><?php echo $veicolo["casa_produttrice"]." - ".$veicolo["modello"]." (".$veicolo["anno_produzione"].")"?></option>
 					<?php endforeach; endif; ?>
 				</select>
 			</li>
@@ -75,14 +75,21 @@
 				<input type="number" name="costo_totale" required >
 			</li>
 			<?php endif;?>
-
-
 		</ul>		
 		<input type="submit" name=<?php if($_SESSION["selezionatoVeicolo"]): echo "inserisciRiparazione"; else: echo "avanti"; endif?> value='<?php if($_SESSION["selezionatoVeicolo"]): echo "Inserisci riparazione";  else: echo "Avanti";endif;?>' >
 	</form>
-
 </section>
-
+<section>
+	<form action="#" method="POST">
+		<ul>
+			<li>
+				<label for="annoRicerca"> Selezionare l'anno in cui si intede ricercare la casa produttrice con un numero di riparazioni superiori a 5: </label>
+				<input type="number" min="1900" max="2023"  step="1" name="annoRicerca" placeholder="Anno" required>
+			</li>
+		</ul>
+		<input type="submit" name="ricerca" value="ricerca" >
+	</form>
+</section>
 <section class="tabelle">
 	<h2>Riparazioni registrate</h2>
 	<table>
@@ -95,7 +102,7 @@
 		<?php foreach($SetParameters["riparazioni"] as $riparazione) :?>
 				<tr class="noform">
 					<td><?php echo $db->getUser($riparazione["CF_cliente"])[0]["nome"]." ".$db->getUser($riparazione["CF_cliente"])[0]["cognome"]; ?></td>
-					<td><?php echo $db->getVeicoloUsatoSpecifico($riparazione["cod_veicolo"]); ?></td>
+					<td><?php echo $db->getVeicoloUsatoSpecifico($riparazione["targa"]); ?></td>
 					<td><?php echo $riparazione["data_inizio"]; ?></td>
 					<td><?php echo $riparazione["data_fine"]; ?></td>
 					<td><?php echo $riparazione["costo_totale"]; ?></td>
